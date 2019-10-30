9880b2e87076efd8937f6799a1108e084c820fb1
Nemanja <nemanjakostovski@gmail.com>
2019-10-30 14:02:09 +0100

    Mini izmene

--- Nedelja2/dan3/zadatak2.js
+++
@@ -1,3 +0,0 @@
-var domValue = 'Welcome to DOM';
-var dom = document.getElementById('dom').innerHTML = (domValue);
-var divAdd = document.getElementsByTagName('DIV')[5].innerHTML = ("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates recusandae necessitatibus earum.");
--- Nedelja2/zadatak4.js
+++ Nedelja2/zadatak4.js
@@ -1,5 +1,5 @@
 for(let i = 1; i <= 5000; i++) {
     if(i % 5 == 0){
-        document.write(i + ', ');
+        console.log(i + ', ');
     }
 }
