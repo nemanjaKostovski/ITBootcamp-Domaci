3552b41d059e49265a4009109b2e8b7bac02593d
Nemanja <nemanjakostovski@gmail.com>
2019-11-03 11:53:44 +0100

    Izmena na zadatku 7

--- Nedelja2/zadatak7.js
+++ Nedelja2/zadatak7.js
@@ -1,10 +1,9 @@
-let n = 5;
 
-for (let i = 1; i <= n; i++) {
+    let n = 5;
     let row = '';
 
-    for (let j = 1; j <= i; j++) {
-        row += '#';
+    for (let i = 1; i <= n; i++) {
+            row += '#';
+        
+        console.log(row);
     }
-    console.log(row);
-}
